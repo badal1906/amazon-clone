5f3a9e5d1eb64739ef38ff0c2b50bc6411d7a05d
badal <badal1906@gmail.com>
2021-11-04 17:47:23 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBRTY_mqadGFi-CrHqHgjnkrKhkRhsQ2_c",
+    authDomain: "clone-42d09.firebaseapp.com",
+    projectId: "clone-42d09",
+    storageBucket: "clone-42d09.appspot.com",
+    messagingSenderId: "547890518564",
+    appId: "1:547890518564:web:fff590fa662e114182d6b1"
+  };
