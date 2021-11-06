64501bd7a58ec0f8fb644f5e43cae4ce9828cb2d
badal <badal1906@gmail.com>
2021-11-06 19:48:45 +0530

    checkout completed

--- src/CheckoutProduct.jsx
+++ src/CheckoutProduct.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./CheckoutProduct.css";
+import { useStateValue } from "./StateProvider";
 
 function CheckoutProduct({ id, image, title, price, rating }) {
-  return (
+  
+    const [{basket},dispatch] = useStateValue()
+
+  const removeFromBasket = () => {
+        dispatch({
+            type:"REMOVE_FROM_BASKET",
+            id:id,
+        })
+  }
+  
+    return (
     <div className="checkoutProduct">
       <img className="checkoutProduct__image" src={image} />
 
@@ -19,7 +30,7 @@ function CheckoutProduct({ id, image, title, price, rating }) {
               <p>🌟</p>
             ))}
         </div>
-        <button>Remove from Basket</button>
+        <button onClick={removeFromBasket}>Remove from Basket</button>
       </div>
     </div>
   );
--- src/reducer.jsx
+++ src/reducer.jsx
@@ -3,9 +3,8 @@ export const initialState = {
 };
 
 //selector
-export const getBasketTotal = (basket) => 
-  basket?.reduce((amount,item) => item.price + amount, 0)
-
+export const getBasketTotal = (basket) =>
+  basket?.reduce((amount, item) => item.price + amount, 0);
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -14,10 +13,28 @@ const reducer = (state, action) => {
         ...state,
         basket: [...state.basket, action.item],
       };
+
+    case "REMOVE_FROM_BASKET":
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+      let newBasket = [...state.basket];
+
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Cant remove product (id: ${action.id}) as its not in basket`
+        );
+      }
+      return {
+        ...state,
+        basket: newBasket,
+      };
+
     default:
       return state;
   }
 };
-
 
 export default reducer;
